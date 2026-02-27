75a89f00656f2186ae610737d38104f95bf510f4
asdf <temp@temp.com>
2026-02-28 00:56:14 +0900

    change analysis ticker: OPEN → RKT

--- auto_post.py
+++ auto_post.py
@@ -59,7 +59,7 @@ MARKET_TICKERS = {
 
 # 기술적 분석 대상 종목 (여기에 추가/수정하세요)
 ANALYSIS_TICKERS = {
-    'Opendoor Technologies': 'OPEN',
+    'Rocket Companies': 'RKT',
 }
 
 # yfinance 티커 → chart_data 키 매핑
