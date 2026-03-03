a049731146c8088abd955b0c2ccb5c3a7303d4c8
asdf <temp@temp.com>
2026-03-04 02:27:09 +0900

    change analysis ticker: 513A.T → APO

--- auto_post.py
+++ auto_post.py
@@ -59,7 +59,7 @@ MARKET_TICKERS = {
 
 # 기술적 분석 대상 종목 (여기에 추가/수정하세요)
 ANALYSIS_TICKERS = {
-    'Global X Japan Defense Tech ETF': '513A.T',
+    'Apollo Global Management, Inc.': 'APO',
 }
 
 # yfinance 티커 → chart_data 키 매핑
